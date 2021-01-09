7e31dadaa12eec09c1958ac648747caf185f5581
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-09 18:40:25 +0700

    simple route express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello TypeScript Express');
+import express, { Application, Request, Response } from 'express';
+class App {
+  public app: Application;
+
+  constructor() {
+    this.app = express();
+    this.routes();
+  }
+
+  protected routes(): void {
+    this.app.route('/').get((req: Request, res: Response) => {
+      res.send('This route used TS');
+    });
+  }
+}
+
+const PORT: number = 8000;
+const app = new App().app;
+
+app.listen(PORT, () => {
+  console.log(`Application listening in ${PORT}`);
+});
