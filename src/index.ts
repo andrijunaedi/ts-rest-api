039fdf25a3e32a2f9890271b98186ea92b1e604b
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-09 19:59:36 +0700

    Install compression helmet cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
+import compression from 'compression';
+import helmet from 'helmet';
+import cors from 'cors';
 class App {
   public app: Application;
 
@@ -13,6 +16,9 @@ class App {
   protected plugins(): void {
     this.app.use(bodyParser.json());
     this.app.use(morgan('dev'));
+    this.app.use(compression());
+    this.app.use(helmet());
+    this.app.use(cors());
   }
 
   protected routes(): void {
