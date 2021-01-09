0b9d407b9b51daab560bc4c9d748d1fa84e09f5c
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-09 19:28:32 +0700

    Body Parser

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express, { Application, Request, Response } from 'express';
+import bodyParser from 'body-parser';
 class App {
   public app: Application;
 
   constructor() {
     this.app = express();
+    this.plugins();
     this.routes();
+  }
+
+  protected plugins(): void {
+    this.app.use(bodyParser.json());
   }
 
   protected routes(): void {
     this.app.route('/').get((req: Request, res: Response) => {
       res.send('This route used TS');
+    });
+
+    this.app.route('/users').post((req: Request, res: Response) => {
+      res.send(req.body);
     });
   }
 }
