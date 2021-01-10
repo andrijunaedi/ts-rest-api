b85ad3a3ac27c01aa9b9a19d43a7b3ca896e2357
andrijunaedi <andrijunaedi126@gmail.com>
2021-01-10 17:03:15 +0700

    Hash User Password

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from 'express';
+import PasswordHash from '../utils/PasswordHash';
 
 const db = require('../db/models');
 
 class AuthController {
   register = async (req: Request, res: Response): Promise<Response> => {
     const { username, password } = req.body;
-    await db.user.create({ username, password });
+    const hashedPassword: string = await PasswordHash.hash(password);
+
+    await db.user.create({ username, password: hashedPassword });
 
     return res.send('Registration success');
   };
