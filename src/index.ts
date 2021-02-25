f96c8f8afdc1e091fafbc4c172f2a14881a70eec
andrijunaedi <andrijunaedi126@gmail.com>
2021-02-25 17:54:09 +0700

    add PORT from env variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { app } from './app';
 
-const port: number = 8000;
+const port: number | string = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Application listening in ${port}`);
